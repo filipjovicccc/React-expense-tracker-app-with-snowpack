40e4ea79f8c87bfc6c2249f3a7683eb4c99c04a1
johnloccck <filipjovicccc@gmail.com>
2021-11-03 21:11:39 +0100

    update

--- src/components/Expences.jsx
+++ src/components/Expences.jsx
@@ -2,7 +2,22 @@ import React from "react";
 import "./Expences.css";
 
 function Expences() {
-  return <div className="expences"></div>;
+  return (
+    <div className="expences">
+      <div className="expences-area">
+        <div className="date-area">November 3th 2021</div>
+        <div className="price-area">$50</div>
+      </div>
+      <div className="expences-area">
+        <div className="date-area">Octobar 28 2021</div>
+        <div className="price-area">$25</div>
+      </div>
+      <div className="expences-area">
+        <div className="date-area">June 5 2021</div>
+        <div className="price-area">$42</div>
+      </div>
+    </div>
+  );
 }
 
 export default Expences;
